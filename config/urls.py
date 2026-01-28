0311221cff92a15349bf25d288ec53bab368e0ca
unkn0wn-j <wjsrnr470@gmail.com>
2026-01-28 17:15:55 +0900

    Fix: 가상환경 설정 완료, 로그인 리다이렉트 및 한글화 적용

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,16 @@ from django.contrib import admin
 from django.urls import path, include
 from todo.views import todo_list, todo_info
 from users import views as user_views
+from django.shortcuts import redirect # 👈 1. redirect 임포트 확인!
 
 urlpatterns = [
+    # 2. 메인 주소('')로 들어오면 바로 'login'으로 던져버리기
+    path('', lambda r: redirect('login'), name='index'),
+
     path('todo/', todo_list, name='todo_list'),
     path('todo/<int:todo_id>/', todo_info, name='todo_info'),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/login/', user_views.login, name='login'),
-    path('accounts/signup/', user_views.sign_up, name='signup')
+    path('login/', user_views.login, name='login'),
+    path('signup/', user_views.sign_up, name='signup')
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
+
+
 from django.conf import settings
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login as django_login
-
+from django.urls import reverse
 
 def sign_up(request):
     form = UserCreationForm(request.POST or None)
@@ -18,7 +20,7 @@ def login(request):
     form = AuthenticationForm(request, data=request.POST or None)
     if form.is_valid():
         django_login(request, form.get_user())
-        return redirect(settings.LOGIN_REDIRECT_URL)
+        return redirect(reverse('todo_list'))
 
     context = {'form': form}
     return render(request, 'registration/login.html', context)
